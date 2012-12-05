bd7d3888956e5e5e305d25033885df79704af24e
Vincent Agnano <vincent.agnano@scopyleft.fr>
2012-12-05 09:43:28 +0100

    refactored init Arduino and Arduino Mega

---
+++ pyfirmata/arduino.py
@@ -0,0 +1,43 @@
+from boards import BOARDS
+from serial.serialutil import SerialException
+from pyfirmata import Board
+
+
+# shortcut classes
+class ArduinoBoard(Board):
+    def __init__(self, *args, **kwargs):
+        args = list(args)
+        args.append(BOARDS[self.board_type])
+        try:
+            super(ArduinoBoard, self).__init__(*args, **kwargs)
+        except SerialException:
+            logging.error(
+                'No device found at "%s". Make sure your Arduino is connected.' % args[0]
+                )
+    def __str__(self):
+        return 'Arduino %s on %s' % (self.name, self.sp.port)
+
+    @property
+    def board_type(self):
+        return ""
+
+
+class Arduino(ArduinoBoard):
+    """
+    A board that will set itself up as a normal Arduino.
+    """
+    def __str__(self):
+        return 'Arduino %s on %s' % (self.name, self.sp.port)
+
+    @property
+    def board_type(self):
+        return "arduino"
+
+
+class ArduinoMega(Board):
+    """
+    A board that will set itself up as an Arduino Mega.
+    """
+    @property
+    def board_type(self):
+        return "ArduinoMega"
