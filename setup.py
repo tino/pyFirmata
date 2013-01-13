795cd799ead8943d66d2c1fa9ee5857c5286afa5
Tino de Bruijn <tinodb@gmail.com>
2013-01-13 15:01:18 +0100

    Added changes for uploading to PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,31 @@
 
 from distutils.core import setup
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name='pyFirmata',
     version='0.9.5',  # Don't forget to change pyfirmata.__version__!
     description="A Python interface for the Firmata procotol",
+    long_description=long_description,
     author='Tino de Bruijn',
     author_email='tinodb@gmail.com',
     packages=['pyfirmata'],
     include_package_data=True,
     install_requires=['pyserial'],
     zip_safe=False,
+    url='https://github.com/tino/pyFirmata',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+        'Topic :: Home Automation',
+    ],
 )
