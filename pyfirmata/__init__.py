999fef721e9737bc6cc47b7f810a271bf90769f1
Jose Urgiles <jose.urgiles@yahoo.com>
2015-10-20 21:25:14 -0500

    Corrected class names in __init__.py file

--- pyfirmata/__init__.py
+++ pyfirmata/__init__.py
@@ -44,15 +44,14 @@ class ArduinoDue(Board):
     def __str__(self):
         return "Arduino Due {0.name} on {0.sp.port}".format(self)
 
-class nano(Board):
+class ArduinoNano(Board):
     """
     A board that will set itself up as an Arduino Due.
     """
     def __init__(self, *args, **kwargs):
         args = list(args)
-        args.append(BOARDS['nano'])
+        args.append(BOARDS['arduino_nano'])
         super(nano, self).__init__(*args, **kwargs)
 
     def __str__(self):
-        return "Arduino nano {0.name} on {0.sp.port}".format(self)
-        
+        return "Arduino Nano {0.name} on {0.sp.port}".format(self)
