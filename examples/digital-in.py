0d02298701357fefe68369d72cb6a9cc1f8412fb
Bernd Porr <mail@berndporr.me.uk>
2018-09-11 01:09:30 +0100

    Added two examples

---
+++ examples/digital-in.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2012, Fabian Affolter <fabian@affolter-engineering.ch>
+# Copyright (c) 2018, Bernd Porr <mail@berndporr.me.uk>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the pyfirmata team nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import pyfirmata
+
+
+# Adjust that the port match your system, see samples below:
+# On Linux: /dev/tty.usbserial-A6008rIF, /dev/ttyACM0, 
+# On Windows: \\.\COM1, \\.\COM2
+PORT = '/dev/ttyACM0'
+
+# Definition of the analog pin
+PINS = (0, 1, 2, 3)
+
+# Creates a new board 
+board = pyfirmata.Arduino(PORT)
+print("Setting up the connection to the board ...")
+board.samplingOn()
+
+# Setup the digital pin
+digital_0 = board.get_pin('d:6:i')
+digital_0.enable_reporting()
+
+while (True):
+    if str(digital_0.read()) == 'True':
+        print("Button pressed")
+    elif str(digital_0.read()) == 'False':
+        print("Button not pressed")
+    else: 
+        print("Button has never been pressed")
+    board.pass_time(0.5)
+
+board.exit()
