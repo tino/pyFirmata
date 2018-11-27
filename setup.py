c58308860f2273011caf151c9dc15f6b3b112882
Bernd Porr <mail@berndporr.me.uk>
2018-11-27 00:32:12 +0000

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='pyFirmata2',
-    version='1.1.1',
+    version='1.1.2',
     description="Use your Arduino as a data acquisition card under Python",
     long_description=long_description,
     author='Bernd Porr',
