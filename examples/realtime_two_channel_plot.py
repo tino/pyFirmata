615039a2dcaa35e9b4456a62a6adafe2abc38ff5
Bernd Porr <mail@berndporr.me.uk>
2018-11-30 00:00:45 +0000

    Two channel version

---
+++ examples/realtime_two_channel_plot.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Plots channels zero and one in two different windows. Requires pyqtgraph.
+
+"""
+
+import threading
+from time import sleep
+import sys
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+
+import numpy as np
+
+from pyfirmata2 import Arduino
+PORT = Arduino.AUTODETECT
+
+# create a global QT application object
+app = QtGui.QApplication(sys.argv)
+
+# signals to all threads in endless loops that we'd like to run these
+running = True
+
+class QtPanningPlot:
+
+    def __init__(self,title):
+        self.win = pg.GraphicsLayoutWidget()
+        self.win.setWindowTitle(title)
+        self.plt = self.win.addPlot()
+        self.plt.setYRange(-1,1)
+        self.plt.setXRange(0,500)
+        self.curve = self.plt.plot()
+        self.data = []
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(100)
+        self.layout = QtGui.QGridLayout()
+        self.win.setLayout(self.layout)
+        self.win.show()
+        
+    def update(self):
+        self.data=self.data[-500:]
+        if self.data:
+            self.curve.setData(np.hstack(self.data))
+
+    def addData(self,d):
+        self.data.append(d)
+
+# Let's create two instances of plot windows
+qtPanningPlot1 = QtPanningPlot("Arduino 1st channel")
+qtPanningPlot2 = QtPanningPlot("Arduino 2nd channel")
+
+# sampling rate: 100Hz
+samplingRate = 100
+
+# called for every new sample which has arrived from the Arduino
+def callBack(data):
+    # send the sample to the plotwindow
+    qtPanningPlot1.addData(data)
+    ch1 = board.analog[1].read()
+    # 1st sample of 2nd channel might arrive later so need to check
+    if ch1:
+        qtPanningPlot2.addData(ch1)
+
+# Get the Ardunio board.
+board = Arduino(PORT)
+
+# Set the sampling rate in the Arduino
+board.samplingOn(1000 / samplingRate)
+
+# Register the callback which adds the data to the animated plot
+board.analog[0].register_callback(callBack)
+
+# Enable the callback
+board.analog[0].enable_reporting()
+board.analog[1].enable_reporting()
+
+# showing all the windows
+app.exec_()
+
