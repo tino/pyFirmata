244d7696b1474a82c579761ac177469beb73fd08
iagolirapassos <iagolirapassos@gmail.com>
2022-03-10 22:28:09 -0300

    Added baud rate choice

--- pyfirmata/__init__.py
+++ pyfirmata/__init__.py
@@ -1,4 +1,5 @@
 from .boards import BOARDS
+from .baudrate import BAUDRATE
 from .pyfirmata import *  # NOQA
 from serial.tools import list_ports
 
@@ -14,7 +15,9 @@ class FindPort():
 		list = []
 		for poert in list_ports.comports():
 			list.append(poert.device)
-		return list
+		if len(list) > 0:
+			return list
+		return None
 		
 	#Choose port
 	def chooseport(self):
@@ -22,15 +25,52 @@ class FindPort():
 		if len(ports) > 1:
 			cont = 0
 			g="----------------\n"
-			for porta in ports:
-				g+=f"{cont+1}. {porta}\n"
+			for port__ in ports:
+				g+=f"{cont+1}. {port__}\n"
 				cont+=1
 			g+="----------------"
 			print(g)
 			choose = int(input("Choose the port by entering the equivalent number: "))
 			return ports[choose-1]
-		else:
-			return ports[0]
+
+		return ports[0]
+	
+	def centralize(self, value, default:int=10, character:str=None):
+		string = str(value)
+		diff = abs(default - len(string))
+		character = ' ' if character is None else character
+
+		if diff > 0:
+			if diff%2 == 0: 
+				diff/=2; diff=int(diff)
+				string = string + character*diff
+				string = character*diff + string
+			else:
+				diff/=2; diff=int(diff)
+				string = string + character*(diff+1)
+				string = character*diff + string
+
+		return string
+
+	def baudrate(self):
+		baudrate_dict = BAUDRATE['velocity']
+
+		fmt = '+----------+----------+\n'
+		fmt +='|  Option  | baudrate |\n'
+		fmt +='+----------+----------+\n'
+		for (option, rate) in baudrate_dict.items():
+			x = self.centralize(option, 10)
+			y = self.centralize(rate, 10)
+
+			fmt +='|{0}|{1}|\n'.format(x, y)
+			fmt +='+----------+----------+\n'
+
+		fmt+="Choose the baudrate of by the equivalent number: "
+		baudrate__ = input(fmt)
+		
+		if baudrate__ not in BAUDRATE['velocity']: return 57600
+
+		return BAUDRATE['velocity'][str(baudrate__)]
 
 # shortcut classes
 
@@ -40,13 +80,19 @@ class Arduino(Board):
 	"""
 	def __init__(self, *args, **kwargs):
 		if len(args) < 1 or args[0] is None:
-			porta = FindPort()
+			port__ = FindPort()
 			args = []
-			args.append(porta.chooseport())
+			args.append(port__.chooseport())
 		else:
 			args = list(args)
-	
+
+		##add board
 		args.append(BOARDS['arduino'])
+		
+		##add baudrate
+		baudrate=port__.baudrate()
+		args.append(baudrate)
+		print(args)
 		super(Arduino, self).__init__(*args, **kwargs)
 
 	def __str__(self):
@@ -59,12 +105,16 @@ class ArduinoMega(Board):
 	"""
 	def __init__(self, *args, **kwargs):
 		if len(args) < 1 or args[0] is None:
-			porta = FindPort()
+			port__ = FindPort()
 			args = []
-			args.append(porta.chooseport())
+			args.append(port__.chooseport())
 		else:
 			args = list(args)
 		args.append(BOARDS['arduino_mega'])
+
+		baudrate=port__.baudrate()
+		args.append(baudrate)
+
 		super(ArduinoMega, self).__init__(*args, **kwargs)
 
 	def __str__(self):
@@ -77,12 +127,17 @@ class ArduinoDue(Board):
 	"""
 	def __init__(self, *args, **kwargs):
 		if len(args) < 1 or args[0] is None:
-			porta = FindPort()
+			port__ = FindPort()
 			args = []
-			args.append(porta.chooseport())
+			args.append(port__.chooseport())
 		else:
 			args = list(args)
+
 		args.append(BOARDS['arduino_due'])
+
+		baudrate=port__.baudrate()
+		args.append(baudrate)
+
 		super(ArduinoDue, self).__init__(*args, **kwargs)
 
 	def __str__(self):
@@ -95,12 +150,17 @@ class ArduinoNano(Board):
 	"""
 	def __init__(self, *args, **kwargs):
 		if len(args) < 1 or args[0] is None:
-			porta = FindPort()
+			port__ = FindPort()
 			args = []
-			args.append(porta.chooseport())
+			args.append(port__.chooseport())
 		else:
 			args = list(args)
+		
 		args.append(BOARDS['arduino_nano'])
+		
+		baudrate=port__.baudrate()
+		args.append(baudrate)
+
 		super(ArduinoNano, self).__init__(*args, **kwargs)
 
 	def __str__(self):
@@ -112,12 +172,17 @@ class ArduinoLeonardo(Board):
 	"""
 	def __init__(self, *args, **kwargs):
 		if len(args) < 1 or args[0] is None:
-			porta = FindPort()
+			port__ = FindPort()
 			args = []
-			args.append(porta.chooseport())
+			args.append(port__.chooseport())
 		else:
 			args = list(args)
+
 		args.append(BOARDS['arduino_leonardo'])
+
+		baudrate=port__.baudrate()
+		args.append(baudrate)
+
 		super(ArduinoLeonardo, self).__init__(*args, **kwargs)
 
 	def __str__(self):
